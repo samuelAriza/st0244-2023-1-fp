d6c0c5a2ed07d2a16836a2ad1c662765d6d81c81
AndresVelezR <130859337+AndresVelezR@users.noreply.github.com>
2023-06-02 23:38:52 -0500

    Update constraints.py
    Added comments

--- src/constraints.py
+++ src/constraints.py
@@ -1,11 +1,13 @@
 from parse import parse_type_equation, parse_datatype
 
+# Reads the type equations from a file and returns a list of parsed equations
 def get(filename):
     c = []
     with open (filename) as archivo:
         for linea in archivo:
             c.append(parse_type_equation(linea.replace(' ', '').replace('\n', '')))
     return c
+
 
 
 
