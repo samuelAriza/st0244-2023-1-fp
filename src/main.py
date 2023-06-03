31675921c3a03f40cfbb2c86bd12d2892691d198
AndresVelezR <130859337+AndresVelezR@users.noreply.github.com>
2023-06-02 23:39:33 -0500

    Update main.py
    Added comments

--- src/main.py
+++ src/main.py
@@ -2,5 +2,8 @@ from constraints import get
 from unify import *
 import sys
 
+# Gets the constraint set from the input file specified in the command line arguments
 constraint = get(sys.argv[1])
+
+# Performs the unification on the constraint set
 unify(constraint)
