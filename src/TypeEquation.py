3576d1129a7c91a450255dca5a39a6721f72f827
AndresVelezR <130859337+AndresVelezR@users.noreply.github.com>
2023-06-02 23:36:41 -0500

    Update TypeEquation.py
    Added comments

--- src/TypeEquation.py
+++ src/TypeEquation.py
@@ -1,16 +1,22 @@
 class TypeEquation:
+
+    # Initializes a TypeEquation object with the given S and T types
     def __init__(self, S, T):
         self.S = S
         self.T = T
 
+    # Returns a string representation of the type equation in the form "S = T"
     def __str__(self):
         return f'{self.S} = {self.T}'
 
+    # Returns a string representation of the type equation based on the specified number
     def __repr__(self, number):
         if(number == 1):
             return self.S
         else:
             return self.T
+        
+    # Returns an abbreviated string representation of the type equation based on the specified number    
     def __acc__(self, number):
         if(number == 1):
             return f'{self.S}'
